22cea84ef3910a7566488c8d4119f4c088a9c8c1
Paul Braham <paul@localhost.localdomain>
2016-04-01 16:38:44 +0100

    initial commit - no api push code yet

---
+++ sender.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python
+
+import RPi.GPIO as GPIO
+import time, logging, sys
+
+#Set up logging
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+GPIO.setmode(GPIO.BCM)
+
+#Initialise GPIO Inputs
+GPIO.setup(4,GPIO.IN,pull_up_down=GPIO.PUD_UP) #Terminal 1
+GPIO.setup(18,GPIO.IN,pull_up_down=GPIO.PUD_UP) #Terminal 2
+GPIO.setup(17,GPIO.IN,pull_up_down=GPIO.PUD_UP) #Terminal 3
+GPIO.setup(27,GPIO.IN,pull_up_down=GPIO.PUD_UP) #Terminal 8
+
+def main():
+        try:
+                GPIO.add_event_detect(4, GPIO.FALLING, callback=pin4Event, bouncetime=200)
+                GPIO.add_event_detect(18, GPIO.FALLING, callback=pin18Event, bouncetime=200)
+                GPIO.add_event_detect(17, GPIO.FALLING, callback=pin17Event, bouncetime=200)
+                GPIO.add_event_detect(27, GPIO.BOTH, callback=pin27Event, bouncetime=200)
+
+        except KeyboardInterrupt:
+                GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+
+        while True:
+                time.sleep(10)
+def pin4Event(channel):
+        pushMessage("reset")
+        logging.info("Intruder Alarm Aborted")
+
+def pin18Event(channel):
+        pushMessage("confirmed")
+        logging.info("Intruder Alarm Confirmed")
+
+def pin17Event(channel):
+        pushMessage("triggered")
+        logging.info("Intruder Alert Triggered")
+
+def pin27Event(channel):
+    if GPIO.input(27):  
+        pushMessage("arm")
+        logging.info("Alarm Armed")  
+    else:                 
+        pushMessage("disarm")
+        logging.info("Alarm Disarmed") 
+
+def pushMessage(event):
+
+#Invoke main function
+if __name__ == "__main__":
+        main()
